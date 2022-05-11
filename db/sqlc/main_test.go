1387b8dd01e0ff0d581c9b7b1b7b3d09a8452461
Vishal Choudhary <vish9812@gmail.com>
2022-05-11 22:12:06 +0530

    Load config from file

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -3,28 +3,23 @@ package db
 import (
 	"database/sql"
 	"log"
+	"me/mybank/util"
 	"os"
 	"testing"
 
 	_ "github.com/lib/pq"
 )
 
-const (
-	driver = "postgres"
-	src    = "postgres://myuser:mypass@localhost:5432/my_bank?sslmode=disable"
-)
-
 var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	// config, err := util.LoadConfig("../..")
-	// if err != nil {
-	// 	log.Fatal("cannot load config:", err)
-	// }
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
 
-	var err error
-	testDB, err = sql.Open(driver, src)
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
--- main.go
+++ main.go
@@ -5,18 +5,18 @@ import (
 	"log"
 	"me/mybank/api"
 	db "me/mybank/db/sqlc"
+	"me/mybank/util"
 
 	_ "github.com/lib/pq"
 )
 
-const (
-	driver        = "postgres"
-	src           = "postgres://myuser:mypass@localhost:5432/my_bank?sslmode=disable"
-	serverAddress = "localhost:7070"
-)
-
 func main() {
-	conn, err := sql.Open(driver, src)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("Config loading failed: ", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
@@ -24,7 +24,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("server start failed", err)
 	}
